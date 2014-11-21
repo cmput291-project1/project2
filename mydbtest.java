49fb25916c50860c3b890552809a793a4180f90f
slmyers <slmyers@ualberta.ca>
2014-11-21 15:37:33 -0700

    added preferences to track database type globally, and modified mydbtest to take command line arguements. added makefile

--- mydbtest.java
+++ mydbtest.java
@@ -5,6 +5,21 @@ import java.util.Arrays;
 public class mydbtest {
 	public static void main(String[] args) {
 		Scan scan = Scan.getInstance();
+		Pref pref = Pref.getInstance();
+		int type = 0; 
+		if (args.length > 0) {
+    		try{
+      		type = Integer.parseInt(args[0]);
+    		}catch (NumberFormatException e) {
+        		System.err.println("Argument" + args[0] + " must be an integer.");
+        		System.exit(-1);
+    		}
+		}
+		else{
+			System.out.println("database type must be selected.");
+			System.exit(-1);
+		}
+		pref.setDbType(type);
 		Menu menu = new Menu();
 		scan.close();
 	}

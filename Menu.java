aae0d1fa1680dfd8664e276da6e24a0f21fb1ed6
slmyers <slmyers@ualberta.ca>
2014-11-22 03:42:52 -0700

    forgot semi-colon

--- Menu.java
+++ Menu.java
@@ -24,7 +24,7 @@ public class Menu{
 		int option = this.select();
 		switch(option){
 			case 1 : 
-						DataBase.getInstance()
+						DataBase.getInstance();
 						break;
 			case 2 : 
 						System.out.println("Option 2 executed");	

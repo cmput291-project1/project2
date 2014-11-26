604f941f56909b27b189d30e26b5671b14fdc1f2
slmyers <slmyers@ualberta.ca>
2014-11-25 23:43:09 -0700

    added DataKeyCreator.java

--- DataKeyCreator.java
+++ DataKeyCreator.java
@@ -13,14 +13,14 @@ public class DataKeyCreator implements SecondaryKeyCreator {
                                       DatabaseEntry result){
 				String dataStr = null;	
 				try{
-					str = new String(data.getData(), "UTF-8");
+					dataStr = new String(data.getData(), "UTF-8");
 				}catch (UnsupportedEncodingException uee){
 					uee.printStackTrace();
 					System.exit(-1);
 				}
- 				int strlen = str.length();
+ 				int strlen = dataStr.length();
 				byte[] byteArray = new byte[strlen];
-				byteArray = str.getBytes();
+				byteArray = dataStr.getBytes();
         result.setData(byteArray);
 				return true;
    }

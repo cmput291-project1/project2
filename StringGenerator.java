bdd5335df3f34a774dff5a912fe3cb583744ff2d
slmyers <slmyers@ualberta.ca>
2014-11-25 18:47:02 -0700

    refactoring string generator

--- StringGenerator.java
+++ StringGenerator.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class StringGenerator{
 	private Random random;
 	private int RANGE = 64; // all strings must be of size 64-127	
-	private StringGenerator gen = null;
+	private static StringGenerator gen = null;
 	
 	protected StringGenerator(){
 		random = new Random(1000000);
@@ -17,7 +17,7 @@ public class StringGenerator{
 	}
 	
 	public final String generateString(){
-		range = 64 + random.nextInt( 64 );
+		int range = 64 + random.nextInt( 64 );
 		String s = "";
 		for ( int j = 0; j < range; j++ ) 
 			s+=(new Character((char)(97+random.nextInt(26)))).toString();

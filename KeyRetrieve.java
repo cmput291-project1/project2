96dbd24eefa9c046e3483d48b8b8d3af6d001aea
Jared Hostyn <jlhostyn@cs.ualberta.ca>
2014-11-28 16:49:55 -0700

    Finished version of key retrieval

--- KeyRetrieve.java
+++ KeyRetrieve.java
@@ -1,83 +1,56 @@
 import com.sleepycat.db.*;
 
-//TODO - Append key/value pair to file named "answers" in local directory
-//     - get IndexFile search working
 public class KeyRetrieve {
-        //example key that has data: thndjefjyfwgpbmhzbfsfkubphiyvqirxwggmuxhvqnfmczshjaldffddmqyylwfmvbttcpvdjjffawzrdmwzykaspfugguntavetgdszamyogibkekcrvjuf
+        //examples 
+	// Random Key: thndjefjyfwgpbmhzbfsfkubphiyvqirxwggmuxhvqnfmczshjaldffddmqyylwfmvbttcpvdjjffawzrdmwzykaspfugguntavetgdszamyogibkekcrvjuf
+	// Random Data: djjtchkroyzbyzqycjbjfvkxuwuywywkcvqltyagjavmhpewjuhfqsaawwzwvusrobrzmkbstekgkbawzkl
+	// Last key: zzzzevhwlhknxjmuxosuaunkzvqaynhihjpryfhwzziekyjpwhtsffnvywrrcwpxpmavfbqzwbxwkwwodfxiwcxsvkfznsgymytctmjleuxohjezlyeodccqletvf
+	// Last Data: woqlahmzrqlyhhjzdklmjqolkbhhiasczpgukpyobcwztbsffleukvbfdnqnubmorshieukeclbbtie
+
+	// DATA BELOW I'M 100% SURE IS RIGHT AS I PRINTED IT AS THE DATABASE WAS BEING CREATED.
+	// key: oohiqwurgzsllzvhgigpxqwzbenyyjxuczmewrecjmxuvgjlzrnfxlmgzoilphatfquyyaadzvnztflneudhykt
+	// data: hnrkmhfcwwzplsykkcaxqdtsnlenyanztgszjgnzvdgpidenkicynfdsgeyvgbnbaxdxlodtwexdlp
+
+	// key: hgfjlbziiqxoqcpdsfginenvozqvyhnsdzwnzmubbxybauzdsolvbtxqqsbdqznvkjixrnaxfmcx
+	// data: zqmornwieprpscrhbldbvrnexrqwxtkzufejctzshhhmblhfiwzwnhcadcotyueskadatwtowrfkxf
+	
+	// key: szysjqctdhewbmldkyzojvnpatqlhofdiavalosfwidkrppmshbcmrihpxuqdmyhvyvpuplerjsqxaseuetlsznwfclfytahorfqfgagjslzkqmlebh
+	// data: fgzqeklegbkhfmvybrrlaeqprkwchikudsvdgksxkoxpmeaeqchluypdvatveqreeevnnbqszeykoyddsflsgnktspcfpgggmvkricdrlzfamhisqyyuljptcsnc
+
 	Scan scan = Scan.getInstance();
 	// creates database or gets instance if it was already created
 	DataBase db = DataBase.getInstance();
-	Database database = db.getPrimaryDb();
+	Database database = null;
+	String filename = "answers.txt";
+    	WriteToFile fileWrite = new WriteToFile();
+	
+	public KeyRetrieve(){	
+		database = db.getPrimaryDb();
+	}
 
 	// main of KeyRetrieve
 	public void getRecords() {
 		// gets user input for record to search for                               
 		System.out.print("Please enter key you want to search for: ");
 		String key = scan.getString();
+		System.out.println(key);
 		
 		// start timer, end before returns
 		long timeStart = System.nanoTime();
+
+		// set up inputed key and data
 		DatabaseEntry dbKey = new DatabaseEntry();
-		DatabaseEntry sKey = new DatabaseEntry();
+		//DatabaseEntry sKey = new DatabaseEntry();
 		dbKey.setData(key.getBytes()); 
 		dbKey.setSize(key.length());
-		
+		System.out.println();
 		DatabaseEntry data = new DatabaseEntry();
-		SecondaryDatabase database2 = null;
-		if (Pref.getDbType() == 3) {
-			//TODO STEVE COMMENTS
-			//use IndexFile.getInstance().getDataSecondary() for the secondary table you wanted
-			//database2 = db.getSecondaryDb();
-		}
 		
 		System.out.println("Searching for key in database");
-		// get the data record using the secondary index if IndexFile database or primary if hash/btree database
+		// get the data record
 		try {
-			if (database2 == null) {
-				database.get(null, dbKey, data, LockMode.DEFAULT);
-			}
-			// may not be correct way to do this
-			else {	
-				//TODO get code underneath to work for multiple instances of a letter being a secondary index key,
-				// code only works for first instance found currently and finding the right key won't work if it's
-				// in a different key of the same letter
-				/*
-				// parses the first char from the given string
-				String secKey = key.substring(0,1);
-				sKey.setData(secKey.getBytes()); 
-				sKey.setSize(secKey.length());
-
-				Cursor c = database2.openSecondaryCursor(null, null);
-				OperationStatus oprStatus = c.getSearchKeyRange(sKey, data, LockMode.DEFAULT);
-				while (oprStatus == OperationStatus.SUCCESS) {
-					String dCheck = new String (data.getData());
-					if (!(dCheck.equals(key))) {
-						oprStatus = c.getNextDup(sKey, data, LockMode.DEFAULT);
-						continue;
-					}
-					else {
-						System.out.println(dCheck);
-						System.out.println("MATCH");
-						break;
-					}
-				}
-				*/
-
-				// Should work but doesn't find anything
-				//database2.getSearchBoth(null, sKey, dbKey, data, LockMode.DEFAULT);
-
-				// only finds first key with correct letter, can't iterate through
-				//database2.get(null, sKey, dbKey, data, LockMode.DEFAULT);
-
-				Cursor c = database2.openSecondaryCursor(null, null);
-				OperationStatus oprStatus = c.getFirst(dbKey, data, LockMode.DEFAULT);
-				while (oprStatus == OperationStatus.SUCCESS) {
-					String s = new String(dbKey.getData());
-					System.out.println(s);
-					oprStatus = c.getNext(dbKey, data, LockMode.DEFAULT);
-				}
-			}		
-		}
+			database.get(null, dbKey, data, LockMode.DEFAULT);
+		}	
 		catch (DatabaseException e) {
 			System.err.println("unable to get key/value record!");
 		}
@@ -89,14 +62,19 @@ public class KeyRetrieve {
 		else {
 			System.out.println("Records found: 1");
 			String sData = new String (data.getData());
-			System.out.println(sData);
+			String fKey = new String (dbKey.getData());
+			System.out.println("Key: " + fKey);
+			System.out.println("Data: " + sData);
+			fileWrite.writeString(filename, fKey); 
+	    		fileWrite.writeString(filename, sData);
+	    		fileWrite.writeString(filename, "");
 		}
 
 		// end timer and print time
 		long timeEnd = System.nanoTime();
     		long time = timeEnd - timeStart;
-		//time = time/1000000;
-    		System.out.println("TOTAL SEARCH TIME = " + time);
+		//time = time/1000;
+    		System.out.println("TOTAL SEARCH TIME = " + time + " Nanoseconds");
 		return;
 
 	}

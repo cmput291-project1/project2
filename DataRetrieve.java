6c44d3be792b0eda8bdc550cd5270953d8545007
slmyers <slmyers@ualberta.ca>
2014-11-29 02:44:43 -0700

    testing and stuff

--- DataRetrieve.java
+++ DataRetrieve.java
@@ -54,6 +54,10 @@ public class DataRetrieve {
 				DatabaseEntry pKey = new DatabaseEntry();
 				DatabaseEntry dataEntry = new DatabaseEntry();
 				//OperationStatus success = ;
+				secKey.setReuseBuffer(false);
+				pKey.setReuseBuffer(false);
+				dataEntry.setReuseBuffer(false);
+				
 				System.out.println("DatabaseEntries are set to reuse byte array, true or false? " + dataEntry.getReuseBuffer());
 	
 				SecondaryDatabase database2 = null;
@@ -74,9 +78,10 @@ public class DataRetrieve {
 							data = new String (secKey.getData());
 							key = new String (pKey.getData());
 							records.put(key, data);
-						secKey = new DatabaseEntry();
-						pKey = new DatabaseEntry();
-						dataEntry = new DatabaseEntry();
+							/*
+							secKey = new DatabaseEntry();
+							pKey = new DatabaseEntry();
+							dataEntry = new DatabaseEntry();*/
 						//next if there are duplicate keys after the first get them 		
 							while(c.getNextDup(secKey, pKey, dataEntry, LockMode.DEFAULT) == OperationStatus.SUCCESS){
 								//if(sData == null){
@@ -85,9 +90,9 @@ public class DataRetrieve {
 								data = new String (secKey.getData());
 								key = new String (pKey.getData());
 								records.put(key, data);
-								secKey = new DatabaseEntry();
+								/*secKey = new DatabaseEntry();
 								pKey = new DatabaseEntry();
-								dataEntry = new DatabaseEntry();
+								dataEntry = new DatabaseEntry();*/
 							}
 						}
 						else{

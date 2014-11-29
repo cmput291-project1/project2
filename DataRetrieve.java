685b42064cc703662d1bc3c3a3ea0938bc6b461b
slmyers <slmyers@ualberta.ca>
2014-11-28 20:22:49 -0700

    working on it

--- DataRetrieve.java
+++ DataRetrieve.java
@@ -27,92 +27,84 @@ public class DataRetrieve {
 
     public DataRetrieve(){
 
-	DataBase db = DataBase.getInstance();
+			DataBase db = DataBase.getInstance();
 	
 
-	if (Pref.getDbType() == 3) {
-	    IndexFile index = IndexFile.getInstance();
-	    index.configureDataSecondary();
-	    db2 = index.getDataSecondary();
+		if (Pref.getDbType() == 3) {
+			  IndexFile index = IndexFile.getInstance();
+			  index.configureDataSecondary();
+			  db2 = index.getDataSecondary();
 
-	}else{
-	    database = db.getPrimaryDb();
-	}	
+		}else{
+			  database = db.getPrimaryDb();
+		}	
 
-    }
+		  }
 
-    //
-    public void getRecords() {
-	// gets user input for record to search for  
-	System.out.print("Please enter data you want to search for: ");
-	String searchData = scan.getString();
+		  //
+		  public void getRecords() {
+				// gets user input for record to search for  
+				System.out.print("Please enter data you want to search for: ");
+				String searchData = scan.getString();
 	
-	// start timer, end before returns
-	long timeStart = System.nanoTime();
-	DatabaseEntry secKey = new DatabaseEntry();
-	DatabaseEntry pKey = new DatabaseEntry();
-	DatabaseEntry dataEntry = new DatabaseEntry();
-	//OperationStatus success = ;
+				// start timer, end before returns
+				long timeStart = System.nanoTime();
+				DatabaseEntry secKey = new DatabaseEntry();
+				DatabaseEntry pKey = new DatabaseEntry();
+				DatabaseEntry dataEntry = new DatabaseEntry();
+				//OperationStatus success = ;
 
 	
-	SecondaryDatabase database2 = null;
-	String sData = null;
-	String data = null;
-	String key = null;
+				SecondaryDatabase database2 = null;
+				String sData = null;
+				String data = null;
+				String key = null;
 		
-	System.out.println("Searching for data in database");
-	
-	try {
-	    //if there is a index file then we can use key search
-	    if (Pref.getDbType() == 3) {
+				System.out.println("Searching for data in database");
 				System.out.println(Pref.getDbType());
-				System.out.println("searching in index file");
-		SecondaryCursor c = db2.openSecondaryCursor(null, null);
-		//set dbKey to the data value we are searching for then move cursor
-		secKey.setData(searchData.getBytes());
-		secKey.setSize(searchData.length());
-		if(c.getSearchKey(secKey, pKey, dataEntry, LockMode.DEFAULT)==OperationStatus.SUCCESS){
-		    data = new String (secKey.getData());
-		    key = new String (pKey.getData());
-		    
-		    records.put(key, data);
+				try {
+					//if there is a index file then we can use key search
+					if (Pref.getDbType() == 3) {
+						System.out.println(Pref.getDbType());
+						System.out.println("searching in index file");
+						SecondaryCursor c = db2.openSecondaryCursor(null, null);
+						//set dbKey to the data value we are searching for then move cursor
+						secKey.setData(searchData.getBytes());
+						secKey.setSize(searchData.length());
+						if(c.getSearchKey(secKey, pKey, dataEntry, LockMode.DEFAULT)==OperationStatus.SUCCESS){
+							data = new String (secKey.getData());
+							key = new String (pKey.getData());
+						
+							records.put(key, data);
 
-		    //next if there are duplicate keys after the first get them 		
-		    while(c.getNextDup(secKey, pKey, dataEntry, LockMode.DEFAULT) == OperationStatus.SUCCESS){
-					//if(sData == null){
-					//    break;
-					//}
-					data = new String (secKey.getData());
-					key = new String (pKey.getData());
-			
-			    records.put(key, data);
-		
-		    }
-		}
-		c.close();
-		
-	    }else{    
-		//if BTREE or HASH then search all records using cursor and return matches 
-		Cursor c = database.openCursor(null, null);
-		c.getFirst(pKey, dataEntry, LockMode.DEFAULT);
-	    	 
-		for(int i = 0; i < 100000; i++){
-		    data = new String (dataEntry.getData());
-		    key = new String (pKey.getData());
-		    
-		
-		    if(sData.equals(searchData)){
-					records.put(key, data);
-		    }
-		    c.getNext(pKey, dataEntry, LockMode.DEFAULT);
-		}
-		c.close();
-	    }
-	    
-	}
-	catch (DatabaseException e) {
-		System.err.println("unable to get key/value record!");
-	}
+						//next if there are duplicate keys after the first get them 		
+							while(c.getNextDup(secKey, pKey, dataEntry, LockMode.DEFAULT) == OperationStatus.SUCCESS){
+								//if(sData == null){
+								//    break;
+								//}
+								data = new String (secKey.getData());
+								key = new String (pKey.getData());
+								records.put(key, data);
+							}
+					}else{    
+						//if BTREE or HASH then search all records using cursor and return matches 
+						Cursor c = database.openCursor(null, null);
+						c.getFirst(pKey, dataEntry, LockMode.DEFAULT);
+						 
+						for(int i = 0; i < 100000; i++){
+								data = new String (dataEntry.getData());
+								key = new String (pKey.getData());
+								if(sData.equals(searchData)){
+									records.put(key, data);
+								}
+							c.getNext(pKey, dataEntry, LockMode.DEFAULT);
+						}
+					}
+				c.close();    
+			}
+			catch (DatabaseException e) {
+				System.err.println("unable to get key/value record!");
+			}
 	
 	// end timer and print time
 	long timeEnd = System.nanoTime();
